28e9e90d5e2280c8c2bddf63ca8a9f63170dd4b2
henriquesd <hsd.foz@gmail.com>
2024-01-13 10:51:48 +0100

    Created project with examples

---
+++ DotNet8NewDataAnnotations/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using DotNet8NewDataAnnotations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8NewDataAnnotations.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            return Ok("Valid ModelState");
+        }
+    }
+}
---
+++ DotNet8NewDataAnnotations/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet8NewDataAnnotations.Models
+{
+    public class Product
+    {
+        [Length(2, 20)]
+        public string Name { get; set; }
+        
+        [Range(1, 1000, MinimumIsExclusive = true, MaximumIsExclusive = false)]
+        public double Price { get; set; }
+
+        [AllowedValues("Computers", "Video Games")]
+        public string Category { get; set; }
+
+        [DeniedValues("Printers")]
+        public string SubCategory { get; set; }
+
+        [Base64String]
+        public string Description { get; set; }
+    }
+}
